16fbc14fed7f92f819066957bb0bcef55693cc73
galAntonis <galant.dev@gmail.com>
2023-10-10 17:25:24 +0300

    Created LoginActivity

---
+++ app/src/main/java/com/galeos/smartalertv2/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.galeos.smartalertv2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setReferences();
+    }
+
+    private void setReferences() {
+    }
+}
